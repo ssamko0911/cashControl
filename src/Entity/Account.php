53e828573c7f90c6e3c259d642c913e0cf666ee8
ssamko0911 <ssamko091184@gmail.com>
2024-08-01 21:55:54 +0100

    Remove AccountRepository (buggy)

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -1,12 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
-use App\Repository\AccountRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: AccountRepository::class)]
 final class Account
 {
-
 }
