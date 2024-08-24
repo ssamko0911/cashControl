20b3d972588f063c920cd9edf8e1efb98748ddef
ssamko0911 <ssamko091184@gmail.com>
2024-08-24 19:43:33 +0100

    Update AccessGroup

--- src/Security/AccessGroup.php
+++ src/Security/AccessGroup.php
@@ -7,7 +7,12 @@ namespace App\Security;
 class AccessGroup
 {
     // Account
-    public const ACCOUNT_READ = 'account:read';
-    public const ACCOUNT_EDIT = 'account:edit';
-    public const ACCOUNT_CREATE = 'account:create';
+    public const string ACCOUNT_READ = 'account:read';
+    public const string ACCOUNT_EDIT = 'account:edit';
+    public const string ACCOUNT_CREATE = 'account:create';
+
+    // Transaction
+    public const string TRANSACTION_READ = 'transaction:read';
+    public const string TRANSACTION_EDIT = 'transaction:edit';
+    public const string TRANSACTION_CREATE = 'transaction:create';
 }
