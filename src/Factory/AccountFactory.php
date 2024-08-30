09dd807810cbce74971585acc4e2e90203982056
ssamko0911 <ssamko091184@gmail.com>
2024-08-30 17:17:10 +0100

    Create AccountFactory

---
+++ src/Factory/AccountFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Account;
+use App\Entity\Enum\AccountTypeEnum;
+use Money\Currency;
+use Money\Money;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+class AccountFactory extends PersistentProxyObjectFactory
+{
+
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->word,
+            'accountType' => self::faker()->randomElement(AccountTypeEnum::cases()),
+            'description' => self::faker()->text(100),
+            'total' => $this->getMoney(),
+       ];
+    }
+
+    protected function getMoney(): Money
+    {
+        return new Money(
+            self::faker()->randomNumber(3),
+            new Currency(
+                self::faker()->currencyCode()
+            )
+        );
+    }
+
+    public static function class(): string
+    {
+        return Account::class;
+    }
+}
