9aa97f8258b74374410bf7b496df55b7e065b535
ssamko0911 <ssamko091184@gmail.com>
2024-09-11 21:30:16 +0100

    Update TransactionListManager

--- src/Manager/TransactionListManager.php
+++ src/Manager/TransactionListManager.php
@@ -3,16 +3,19 @@
 namespace App\Manager;
 
 use App\Builder\TransactionEntityBuilder;
-use App\DTO\TransactionDTO;
 use App\Entity\Account;
+use App\Repository\TransactionRepository;
 use App\Service\PaginationService;
+use DateTimeImmutable;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\Request;
 
 final readonly class TransactionListManager
 {
     public function __construct(
         private TransactionEntityBuilder $builder,
-        private PaginationService $paginationService
+        private PaginationService $paginationService,
+        private TransactionRepository $transactionRepository
     ) {
     }
 
@@ -23,9 +26,24 @@ final readonly class TransactionListManager
      */
     public function getList(Request $request, Account $account): array
     {
+        //TODO: split into smaller chunks;
+        $qb = $this->transactionRepository->createQueryBuilder('t');
+
+        $this->joinAccount($qb);
+
+        $qb
+            ->where('t.account = :account')
+            ->setParameter('account', $account)
+            ->groupBy('t')
+            ->orderBy('t.id')
+            ->getQuery();
+
+        $this->applyDateRangeFilter($qb, $request);
+
         $transactions = $this->paginationService->paginate(
             $account,
-            $request->query->getInt('page', 1)
+            $request->query->getInt('page', 1),
+            $qb
         );
 
         $transactionDtos = [];
@@ -37,5 +55,30 @@ final readonly class TransactionListManager
             'data' => $transactionDtos,
             'meta' => $transactions['meta'],
         ];
+    }
+
+    private function applyDateRangeFilter(QueryBuilder $qb, Request $request): void
+    {
+        $startDate = $request->query->get('startDate');
+        $endDate = $request->query->get('endDate');
+
+        if (null !== $startDate) {
+            $qb
+                ->andWhere('t.createdAt >= :startDate')
+                ->setParameter('startDate', $startDate);
+        }
+
+        if (null !== $endDate) {
+            $qb
+                ->andWhere('t.createdAt <= :endDate')
+                ->setParameter('endDate', $endDate);
+        }
+    }
+
+    private function joinAccount(QueryBuilder $qb): void
+    {
+        if (!in_array('acc', $qb->getAllAliases(), true)) {
+            $qb->join(Account::class, 'acc');
+        }
     }
 }
