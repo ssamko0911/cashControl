d7112c023c74e73e0f30bc178495f9500599e0c7
ssamko0911 <ssamko091184@gmail.com>
2024-10-02 20:33:29 +0100

    Create Category

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Factory\CategoryFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        CategoryFactory::createMany(5);
+
+        $manager->flush();
+    }
+}
---
+++ src/Factory/CategoryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Category;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+class CategoryFactory extends PersistentProxyObjectFactory
+{
+
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->unique()->word(),
+            'description' => self::faker()->text(100),
+        ];
+    }
+
+    public static function class(): string
+    {
+        return Category::class;
+    }
+}
