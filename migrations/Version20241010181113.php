0b163e8fd037427d3f56ce33a239b6a0a80220e2
ssamko0911 <ssamko091184@gmail.com>
2024-10-12 12:14:22 +0100

    Update migration

--- migrations/Version20241010181113.php
+++ migrations/Version20241010181113.php
@@ -7,25 +7,20 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20241010181113 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'rename limit - fails as this one is reserved';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category_budget CHANGE `limit` budget_limit JSON NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category_budget CHANGE budget_limit `limit` JSON NOT NULL');
     }
 }
