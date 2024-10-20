1e239b44167448703a478c13306a064e09fd9be8
ssamko0911 <ssamko091184@gmail.com>
2024-10-20 12:49:37 +0100

    Update AccountManager

--- src/Manager/AccountManager.php
+++ src/Manager/AccountManager.php
@@ -9,18 +9,27 @@ use App\Builder\TransactionEntityBuilder;
 use App\DTO\AccountDTO;
 use App\DTO\TransactionDTO;
 use App\Entity\Account;
+use App\Entity\CategoryBudget;
+use App\Entity\Enum\TransactionType;
 use App\Entity\Transaction;
+use App\Repository\CategoryBudgetRepository;
+use App\Repository\TransactionRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Money\Currency;
+use Money\Money;
 use Psr\Log\LoggerInterface;
 
 final readonly class AccountManager
 {
     public function __construct(
-        private EntityManagerInterface $em,
-        private AccountEntityBuilder $accountEntityBuilder,
+        private EntityManagerInterface   $em,
+        private AccountEntityBuilder     $accountEntityBuilder,
         private TransactionEntityBuilder $transactionEntityBuilder,
-        private LoggerInterface $logger
-    ) {
+        private LoggerInterface          $logger,
+        private TransactionRepository    $transactionRepository,
+        private CategoryBudgetRepository $categoryBudgetRepository
+    )
+    {
     }
 
     public function saveAccount(AccountDTO $dto): Account
@@ -44,13 +53,51 @@ final readonly class AccountManager
         $this->em->persist($transaction);
         $this->em->flush();
 
-        $this->logger->info('Transaction has been created', [
-            'id' => $transaction->getId(),
+        /** @var Transaction|null $previousTransaction */
+        $previousTransaction = $this->transactionRepository->find($transaction->getId() - 1);
+        $currentCategoryBudget = $this->categoryBudgetRepository->findBy(['category' => $categoryId]);
+
+        if (null !== $previousTransaction &&
+            ($previousTransaction->getCreatedAt()->format('n') !== $transaction->getCreatedAt()->format('n'))) {
+            $categoryBudget = new CategoryBudget();
+            $amount = $dto->amount->amount;
+            $currency = new Currency($dto->amount->currency->code);
+            $categoryBudget->setCurrentSpending(new Money($amount, $currency));
+            $this->updateCategoryBudget($categoryBudget, $transaction);
+
+        }
+
+
+        $this->logger->info('Transaction has been created', ['id' => $transaction->getId(),
             'description' => $transaction->getDescription(),
             'account id' => $transaction->getAccount()->getId(),
-            'time' => $transaction->getCreatedAt(),
-        ]);
+            'time' => $transaction->getCreatedAt(),]);
 
         return $transaction;
     }
+
+    private function updateCategoryBudget(CategoryBudget $categoryBudget, Transaction $transaction): void
+    {
+        if ($transaction->getType() === TransactionType::TYPE_EXPENSE) {
+            $newAmount = $categoryBudget->getCurrentSpending()->add($transaction->getAmount());
+        } else {
+            $newAmount = $categoryBudget->getCurrentSpending()->subtract($transaction->getAmount());
+        }
+
+        $categoryBudget->setCurrentSpending($newAmount);
+
+        if ($newAmount->compare($categoryBudget->getBudgetLimit()) >= 0) {
+            $categoryBudget->setIsOverBudget(true);
+        }
+    }
+
+    public function isNextMonth(\DateTimeImmutable $date1, \DateTimeImmutable $date2): bool
+    {
+        $nextMonth = (clone $date1)->modify('first day of next month');
+        $lastDayOfNextMonth = (clone $nextMonth)->modify('last day of this month');
+
+        return $date2 >= $nextMonth && $date2 <= $lastDayOfNextMonth;
+    }
+
+
 }
