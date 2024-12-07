a26b42d7fb3b568de3f95a77c53f90f73fc75650
ssamko0911 <ssamko091184@gmail.com>
2024-12-07 19:05:12 +0000

    Update CategoryDTO

--- src/DTO/CategoryDTO.php
+++ src/DTO/CategoryDTO.php
@@ -15,26 +15,46 @@ use OpenApi\Attributes\Property;
 use OpenApi\Attributes\Items;
 use Nelmio\ApiDocBundle\Annotation\Model;
 
-#[Groups([
-    AccessGroup::CATEGORY_CREATE,
-    AccessGroup::CATEGORY_READ,
-    AccessGroup::CATEGORY_EDIT,
-    AccessGroup::TRANSACTION_READ,
-])]
 final class CategoryDTO implements DTOInterface
 {
+
+    #[Groups([
+        AccessGroup::CATEGORY_READ,
+        AccessGroup::CATEGORY_EDIT,
+        AccessGroup::TRANSACTION_READ,
+    ])]
     public int $id;
 
+
+    #[Groups([
+        AccessGroup::CATEGORY_CREATE,
+        AccessGroup::CATEGORY_READ,
+        AccessGroup::CATEGORY_EDIT,
+        AccessGroup::TRANSACTION_READ,
+    ])]
     public string $name;
 
+
+    #[Groups([
+        AccessGroup::CATEGORY_CREATE,
+        AccessGroup::CATEGORY_READ,
+        AccessGroup::CATEGORY_EDIT,
+        AccessGroup::TRANSACTION_READ,
+    ])]
     public string $description;
 
+
+    // TODO: EDIT
+    #[Groups([
+        AccessGroup::CATEGORY_READ,
+        AccessGroup::TRANSACTION_READ,
+    ])]
     #[Assert\All(new Assert\Type(type: CategoryBudget::class))]
     #[Property(
         type: 'array',
         items: new Items(ref: new Model(type: CategoryBudget::class)),
     )]
-    /** @var CategoryBudgetDTO[] $expirations */
+    /** @var CategoryBudgetDTO[] $monthlyBudgets */
     public array $monthlyBudgets;
 
     #[Ignore] public function getEntityObject(): EntityInterface
